3124c510d30b15ded0ceb5d4e160aa401c7a81d8
Preetha <palanisamypreetha500@gmail.com>
2026-07-17 02:03:15 -0700

    main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from app.routes.state import router as state_router
 from app.routes.gift_master import router as gift_router
 from app.routes.users import router as user_router
 from app.routes.pricing_details import router as pricing_router
-
+from routes.auth import router as auth_router
 
 
 Base.metadata.create_all(bind=engine)
@@ -26,3 +26,4 @@ app.include_router(state_router)
 app.include_router(gift_router)
 app.include_router(user_router)
 app.include_router(pricing_router)
+app.include_router(auth_router)
