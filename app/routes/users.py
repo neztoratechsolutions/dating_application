df649a66f3e0c8d1f45a5bd76bdc4d105bc7476f
Preetha <palanisamypreetha500@gmail.com>
2026-07-17 00:07:37 -0700

    users.py

---
+++ app/routes/users.py
@@ -0,0 +1,131 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal
+from app.models.users import User
+from app.schemas.users import (UserCreate,UserUpdate,UserResponse)
+from app.security import (hash_password,generate_referral_code)
+
+router = APIRouter(prefix="/users",tags=["Users"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/", response_model=UserResponse)
+def create_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+
+    new_user = User(
+        email=user.email,
+        phone=user.phone,
+        password=hash_password(user.password),
+        display_name=user.display_name,
+        bio=user.bio,
+        description=user.description,
+        state_id=user.state_id,
+        profile_photo=user.profile_photo,
+        referral_code=generate_referral_code()
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+@router.get("/", response_model=list[UserResponse])
+def get_users(
+    db: Session = Depends(get_db)
+):
+    return db.query(User).all()
+
+
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(
+        User.id == user_id
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return user
+
+
+@router.put("/{user_id}", response_model=UserResponse)
+def update_user(
+    user_id: int,
+    data: UserUpdate,
+    db: Session = Depends(get_db)
+):
+
+    user = db.query(User).filter(
+        User.id == user_id
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    update_data = data.model_dump(
+        exclude_unset=True
+    )
+
+    for key, value in update_data.items():
+        setattr(user, key, value)
+
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+@router.delete("/{user_id}")
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+
+    user = db.query(User).filter(
+        User.id == user_id
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    db.delete(user)
+    db.commit()
+
+    return {
+        "message": "User deleted successfully"
+    }
