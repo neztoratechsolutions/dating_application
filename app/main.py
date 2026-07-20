b4fe2b488548d0cc0dbe1a69836f1536d78574ac
Preetha <palanisamypreetha500@gmail.com>
2026-07-20 04:04:33 -0700

    main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from models.gift_master import GiftMaster
 from models.users import User
 from models.pricing_details import PricingDetail
 from models.user_status import UserStatus
+from models.gallery import Gallery
 
 
 from routes.state import router as state_router
@@ -14,6 +15,7 @@ from routes.users import router as user_router
 from routes.pricing_details import router as pricing_router
 from routes.auth import router as auth_router
 from routes.user_status_create import router as user_status_router
+from routes.gallery import router as gallery_router
 
 
 
@@ -30,3 +32,4 @@ app.include_router(user_router)
 app.include_router(pricing_router)
 app.include_router(auth_router)
 app.include_router(user_status_router)
+app.include_router(gallery_router)
