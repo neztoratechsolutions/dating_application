411339945b9bba7ca3a49dac0fd986d72a49ddc1
Preetha <palanisamypreetha500@gmail.com>
2026-07-20 00:04:54 -0700

    auth.py

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -3,3 +3,18 @@ from pydantic import BaseModel, EmailStr
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr
+
+
+class VerifyOTPRequest(BaseModel):
+    email: EmailStr
+    otp: str
+
+
+class ResetPasswordRequest(BaseModel):
+    email: EmailStr
+    otp: str
+    new_password: str
